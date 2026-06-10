7f2811e79d306b3180ba29d546afd5c330796a98
3900563672 <lb3900563672@qq.com>
2026-06-10 20:19:32 +0800

    接入原生httputil.ReverseProxy，实现网关的核心转发与响应拦截能力

--- main.go
+++ main.go
@@ -1,17 +1,70 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
-//限流器
-
 var globalLimiter = rate.NewLimiter(rate.Every(330*time.Millisecond), 3)
+
+type contextKey string
+
+const traceIDKey contextKey = "traceID"
+
+func NewGatewayProxy(target string) *httputil.ReverseProxy {
+	targetURL, err := url.Parse(target)
+	if err != nil {
+		log.Fatalf("解析后端目标地址失败: %v", err)
+	}
+
+	proxy := &httputil.ReverseProxy{}
+
+	proxy.Rewrite = func(pr *httputil.ProxyRequest) {
+		pr.SetURL(targetURL)
+
+		pr.Out.Host = targetURL.Host
+	}
+	//钩子
+	proxy.ModifyResponse = func(resp *http.Response) error {
+		traceID, _ := resp.Request.Context().Value(traceIDKey).(string)
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("读取后端响应失败: %v", err)
+		}
+		resp.Body.Close()
+
+		isValidJSON := json.Valid(bodyBytes)
+		log.Printf("[%s] 🔙 哨兵拦截成功！后端真实响应状态码: %d, 是否为合法JSON: %t", traceID, resp.StatusCode, isValidJSON)
+
+		if resp.StatusCode >= 400 {
+			log.Printf("[%s] 🚨 警告：检测到微服务状态异常(%d)！自愈引擎准备介入...", traceID, resp.StatusCode)
+
+			healedData := []byte(`{"status":"HEALED","message":"检测到微服务崩溃，SmartShield网关已采用最新Rewrite引擎全自动自愈！"}`)
+			bodyBytes = healedData
+
+			resp.StatusCode = http.StatusOK
+			resp.Status = "200 OK"
+		}
+		resp.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		resp.ContentLength = int64(len(bodyBytes))
+		resp.Header.Set("Content-Length", fmt.Sprintf("%d", len(bodyBytes)))
+
+		return nil
+	}
+	return proxy
+}
+
+//限流器
 
 func rateLimitMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -47,9 +100,13 @@ func traceMiddleware(next http.HandlerFunc) http.HandlerFunc {
 }
 func main() {
 
-	http.HandleFunc("/", traceMiddleware((rateLimitMiddleware(handleProxy))))
+	targetBackend := "http://httpbin.org"
+	gatewayProxy := NewGatewayProxy(targetBackend)
 
-	log.Println("🚀 SmartShield Gateway MVP 正在启动，监听端口 :8080...")
+	http.HandleFunc("/", traceMiddleware(rateLimitMiddleware(gatewayProxy.ServeHTTP)))
+	log.Printf("🚀 SmartShield Gateway v0.3.0 正在启动...")
+	log.Printf("监听端口 :8080 -> 自动反向代理至: %s", targetBackend)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("网关启动异常: %v", err)
 	}
