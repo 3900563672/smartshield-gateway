c009cd7b9b162f1158a2fb11e46a92cf640525eb
3900563672 <lb3900563672@qq.com>
2026-06-10 19:45:30 +0800

    实现基于Channel的手写令牌桶限流与链路追踪MVP(极其简单版)

--- main.go
+++ main.go
@@ -7,6 +7,57 @@ import (
 	"time"
 )
 
+// 令牌桶
+type TokenBucketLimiter struct {
+	tokens chan struct{}
+}
+
+func NewTokenBucketLimiter(maxTokens int, refillRate time.Duration) *TokenBucketLimiter {
+
+	limiter := &TokenBucketLimiter{
+		tokens: make(chan struct{}, maxTokens),
+	}
+
+	for i := 0; i < maxTokens; i++ {
+		limiter.tokens <- struct{}{}
+	}
+
+	go func() {
+		ticker := time.NewTicker(refillRate)
+		for range ticker.C {
+			select {
+			case limiter.tokens <- struct{}{}:
+			default:
+			}
+		}
+	}()
+	return limiter
+}
+
+func (tl *TokenBucketLimiter) Allow() bool {
+	select {
+	case <-tl.tokens:
+		return true
+	default:
+		return false
+	}
+}
+
+var globalLimiter = NewTokenBucketLimiter(3, 330*time.Millisecond)
+
+func rateLimitMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !globalLimiter.Allow() {
+			// 没令牌了，直接拦截，返回 429 状态码
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusTooManyRequests)
+			w.Write([]byte(`{"error": "Too Many Requests", "message": "网关流量触发熔断，请稍后再试"}`))
+			return
+		}
+		next(w, r)
+	}
+}
+
 // 编写核心代码处理器(自愈)
 func handleProxy(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -29,7 +80,7 @@ func traceMiddleware(next http.HandlerFunc) http.HandlerFunc {
 }
 func main() {
 
-	http.HandleFunc("/", traceMiddleware(handleProxy))
+	http.HandleFunc("/", traceMiddleware((rateLimitMiddleware(handleProxy))))
 
 	log.Println("🚀 SmartShield Gateway MVP 正在启动，监听端口 :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
